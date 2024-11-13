c14d096e98292f8f64bd120f9af352ccc767e5ca
Tiberiu Popescu <tiberiu1204@gmail.com>
2024-11-13 15:34:02 +0200

    Added descriptions of the products and inserted more products

---
+++ insert.sql
@@ -0,0 +1,27 @@
+select * from django.ecommerce_categorie;
+
+INSERT INTO ecommerce_produs (id, nume_produs, descriere, pret, categorie_id, data_adaugarii)
+VALUES
+(11, 'Telefon mobil Samsung Galaxy S23', 'Samsung Galaxy S23 cu display Dynamic AMOLED 2X de 6.1 inch, procesor Exynos 2200, 8GB RAM, 128GB stocare.', 3999.00, 1, NOW()),
+(12, 'Frigider Samsung RB37J5000SS', 'Frigider cu două uși, capacitate 367 litri, tehnologie No Frost, clasă energetică A++.', 1899.00, 2, NOW()),
+(13, 'Canapea extensibilă IKEA FRIHETEN', 'Canapea extensibilă IKEA FRIHETEN, cadru din lemn, material textil, 2 locuri, culorile disponibile: gri, albastru, negru.', 1300.00, 3, NOW()),
+(14, 'Tricou Nike Air', 'Tricou din bumbac 100%, cu logo Nike Air, croială regular fit, pentru bărbați.', 120.00, 4, NOW()),
+(15, 'Carte "Micul Prinț" de Antoine de Saint-Exupéry', 'Carte clasică pentru copii și adulți, despre aventurile unui prinț dintr-o planetă îndepărtată.', 25.00, 5, NOW()),
+(16, 'Set LEGO Creator Expert', 'Set de construcție LEGO Creator Expert, 1800 piese, pentru vârstele 16+, permite construirea unui model detaliat de mașină sport.', 350.00, 6, NOW()),
+(17, 'Șampon Head & Shoulders', 'Șampon anticalcar, pentru păr normal și scalp sensibil, 400 ml.', 25.00, 7, NOW()),
+(18, 'Pâine integrală, 500g', 'Pâine integrală din făină 100% naturală, ideală pentru alimentație sănătoasă.', 5.00, 8, NOW()),
+(19, 'Răzătoare pentru legume', 'Răzătoare din inox cu 4 funcții, pentru legume, brânză și fructe.', 35.00, 8, NOW()),
+(20, 'Baterie auto Varta Blue Dynamic', 'Baterie auto Varta Blue Dynamic 12V, 60Ah, pentru majoritatea autovehiculelor moderne.', 299.00, 10, NOW()),
+(21, 'Perne decorative pentru canapea', 'Set de 4 perne decorative, diverse culori, din material textil rezistent, ideal pentru orice decor de living.', 90.00, 3, NOW()),
+(22, 'Ceas smartwatch Xiaomi Mi Band 7', 'Ceas inteligent Xiaomi Mi Band 7, cu monitorizare a activității fizice și GPS, pentru Android și iOS.', 199.00, 1, NOW()),
+(23, 'Plasă antigrindină pentru grădină', 'Plasă antigrindină 5x5m, rezistentă, pentru protejarea plantelor în timpul furtunilor cu grindină.', 80.00, 9, NOW()),
+(24, 'Laptop Dell Inspiron 15', 'Laptop Dell Inspiron 15, procesor Intel i5, 8GB RAM, 512GB SSD, display Full HD de 15.6 inch.', 2200.00, 1, NOW()),
+(25, 'Aer condiționat portabil', 'Aer condiționat portabil cu 3 funcții: răcire, dezumidificare, ventilație, eficient pentru camere de până la 25 mp.', 1000.00, 2, NOW()),
+(26, 'Bicicletă MTB Merida Big Seven 400', 'Bicicletă montană Merida Big Seven 400, 27.5 inch, schimbătoare Shimano, cadru din aluminiu.', 1450.00, 9, NOW()),
+(27, 'Jucărie educativă pentru bebeluși', 'Jucărie din lemn pentru bebeluși, cu forme geometrice și culori vii, pentru învățare timpurie.', 50.00, 6, NOW()),
+(28, 'Căști audio Sony WH-1000XM4', 'Căști over-ear Sony WH-1000XM4 cu anulare activă a zgomotului, sunet de înaltă calitate.', 950.00, 1, NOW()),
+(29, 'Set de 3 oale pentru gătit', 'Set de 3 oale din inox pentru gătit, cu capace și mânere rezistente la căldură.', 250.00, 2, NOW()),
+(30, 'Căciulă de iarnă unisex', 'Căciulă din lână, unisex, pentru sezonul rece, disponibilă în mai multe culori.', 40.00, 4, NOW()),
+(31, 'Televizor Samsung 4K UHD', 'Televizor Samsung 4K UHD de 55 inch, Smart TV, cu aplicații și tehnologie HDR.', 3000.00, 1, NOW()),
+(32, 'Pachet starter pentru grădinărit', 'Pachet complet pentru grădinărit, incluzând semințe de flori și legume, ustensile și ghivece.', 100.00, 9, NOW());
+
